fc49b38079a8970665ec7a022ef91429c8a028be
RotsenCH <nestor.chumania@epn.edu.ec>
2023-09-04 16:38:45 -0500

    Prueba de Listar

--- src/tests/Paciente.test.js
+++ src/tests/Paciente.test.js
@@ -18,92 +18,53 @@ afterAll(async () => {
 
 describe('Controlador de Pacientes', () => {
     it('Debería listar pacientes correctamente', async () => {
-        // Datos de paciente de prueba
-        const datosPacientePrueba = {
-            nombre: "Nombre del Paciente",
-            propietario: "Nombre del Propietario",
-            email: "correo@ejemplo.com",
-            celular: "1234567890",
-            convencional: "9876543210",
+        // Crear un paciente de prueba en la base de datos
+        const nuevoPaciente = new Paciente({
+            nombre: 'Paciente de prueba',
+            propietario: 'Propietario de prueba',
+            email: 'correo@example.com',
+            celular: '123456789',
+            convencional: '987654321',
             ingreso: new Date(),
-            sintomas: "Síntomas del paciente de prueba",
+            sintomas: 'Síntomas de prueba',
+            veterinario: new mongoose.Types.ObjectId(),
             estado: true,
-        };
+        });
+        await nuevoPaciente.save();
 
-        // Configuración del mock de Paciente para devolver los datos de prueba
-        const mockPaciente = {
-            find: jest.fn(() => [
-                {
-                    _id: "ID_GENERADO_POR_MONGODB",
-                    nombre: "Nombre del Paciente",
-                    propietario: "Nombre del Propietario",
-                    email: "correo@ejemplo.com",
-                    celular: "1234567890",
-                    convencional: "9876543210",
-                    ingreso: new Date(),
-                    estado: true,
-                },
-            ]),
+        const req = {
+            veterinarianBDD: nuevoPaciente.veterinario, // Simula la variable de veterinarioBDD en la solicitud
         };
-
-        // Sobrescribe la implementación de Paciente con el mock
-        jest.spyOn(Paciente, 'find').mockImplementation(mockPaciente.find);
-
-        // Mock de la solicitud y respuesta de Express
-        const mockReq = {
-            veterinarioBDD: { _id: "64f54d16ae4e67a1924abe87" }, // Reemplaza con el ID correcto
-        };
-        const mockRes = {
+        const res = {
             status: jest.fn().mockReturnThis(),
             json: jest.fn(),
         };
 
-        // Llamar a la función listarPacientes
-        await listarPacientes(mockReq, mockRes);
+        await listarPacientes(req, res);
 
-        // Verificar que la función de búsqueda se haya llamado con los parámetros correctos
-        expect(Paciente.find).toHaveBeenCalledWith({
-            estado: true,
-            veterinario: mockReq.veterinarioBDD._id,
-        });
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalled();
 
-        // Verificar que se haya respondido con los pacientes
-        expect(mockRes.status).toHaveBeenCalledWith(200);
-        expect(mockRes.json).toHaveBeenCalledWith([
-            {
-                _id: "ID_GENERADO_POR_MONGODB",
-                nombre: "Nombre del Paciente",
-                propietario: "Nombre del Propietario",
-                email: "correo@ejemplo.com",
-                celular: "1234567890",
-                convencional: "9876543210",
-                ingreso: expect.any(Date),
-                estado: true,
-            },
-        ]);
+        // Limpia la base de datos después de la prueba
+        await Paciente.findByIdAndDelete(nuevoPaciente._id);
     });
 
-    // Resto de las pruebas...
+    it('Debería obtener el detalle de un paciente', async () => {
+        // Agregar código de prueba aquí
+    });
 
+    it('Debería registrar un nuevo paciente', async () => {
+        // Agregar código de prueba aquí
+    });
 
-    // Resto de las pruebas...
-    // it('Debería obtener el detalle de un paciente', async () => {
-//     // Agregar código de prueba aquí
-// });
+    it('Debería actualizar los datos de un paciente', async () => {
+        // Agregar código de prueba aquí
+    });
 
-// it('Debería registrar un nuevo paciente', async () => {
-//     // Agregar código de prueba aquí
-// });
-
-// it('Debería actualizar los datos de un paciente', async () => {
-//     // Agregar código de prueba aquí
-// });
-
-// it('Debería eliminar un paciente', async () => {
-//     // Agregar código de prueba aquí
-// });
-// });
-
+    it('Debería eliminar un paciente', async () => {
+        // Agregar código de prueba aquí
+    });
 });
+
 
 
