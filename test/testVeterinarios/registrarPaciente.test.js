0d396e5d24763cfbd91d33439a3eac5c9e105960
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-09-03 23:22:48 -0500

    Configuracion para pruebas unitarias

---
+++ test/testVeterinarios/registrarPaciente.test.js
@@ -0,0 +1,2 @@
+import {registrarPaciente} from '../../src/controllers/paciente_controller.js'; // Importa la función a probar
+import Paciente from '../../src/models/Paciente.js'; // Importa el modelo Paciente
