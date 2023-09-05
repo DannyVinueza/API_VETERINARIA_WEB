2e4a26c61f1bdc2c04c3febd6673caf4a6515e01
Néstor Chumania <nestor.chumania@epn.edu.ec>
2023-09-05 02:08:34 -0500

    Terminadas las pruebas unitarias de Pacientes

--- src/tests/Paciente.test.js
+++ src/tests/Paciente.test.js
@@ -1,9 +1,13 @@
 import mongoose from 'mongoose';
 import Paciente from "../models/Paciente.js"; // Asegúrate de que la ruta sea correcta
+import Veterinario from "../models/Veterinario.js"; // Asegúrate de que la ruta sea correcta
+
 import {
     listarPacientes,
     detallePaciente,
     registrarPaciente,
+    actualizarPaciente,
+    eliminarPaciente
 
 } from "../controllers/paciente_controller.js";
 
@@ -85,6 +89,13 @@ describe('Controlador de Pacientes', () => {
     });
 
     it('Debería registrar un nuevo paciente', async () => {
+        // Crear un veterinario de prueba en la base de datos
+        const veterinarioPrueba = new Veterinario({
+            nombre: 'Veterinario Prueba',
+            // Otros campos de veterinario
+        });
+        await veterinarioPrueba.save();
+
         const req = {
             body: {
                 nombre: 'Paciente de prueba',
@@ -95,32 +106,126 @@ describe('Controlador de Pacientes', () => {
                 ingreso: new Date(),
                 sintomas: 'Síntomas de prueba',
             },
+            // Asigna el _id del veterinario de prueba
             veterinarioBDD: {
-                _id: new mongoose.Types.ObjectId(), // Simula el veterinarioBDD en la solicitud
+                _id: veterinarioPrueba._id,
             },
         };
+
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await registrarPaciente(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({ msg: "Registro exitoso del paciente" });
+
+        // Limpia la base de datos después de la prueba
+        await Paciente.findOneAndDelete({ nombre: 'Paciente de prueba' });
+        await Veterinario.findOneAndDelete({ _id: veterinarioPrueba._id });
+    });
+
+    it('Debería actualizar los datos de un paciente', async () => {
+        // Crear un paciente de prueba en la base de datos
+        const pacientePrueba = new Paciente({
+            nombre: 'Paciente de prueba',
+            propietario: 'Propietario de prueba',
+            email: 'correo@example.com',
+            celular: '123456789',
+            convencional: '987654321',
+            ingreso: new Date(),
+            sintomas: 'Síntomas de prueba',
+            veterinario: new mongoose.Types.ObjectId(), // Simula el ID de un veterinario existente
+        });
+
+        await pacientePrueba.save(); // Guardar el paciente de prueba en la base de datos
+
+        // Simula una solicitud para actualizar el paciente recién creado
+        const req = {
+            params: {
+                id: pacientePrueba._id, // Usamos el ID del paciente de prueba
+            },
+            body: {
+                nombre: 'Nuevo nombre', // Los nuevos datos que deseas actualizar
+                propietario: 'Nuevo propietario',
+                // Otros campos que deseas actualizar
+            },
+        };
+
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await actualizarPaciente(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({ msg: "Actualización exitosa del paciente" });
+
+        // Verifica que los datos se hayan actualizado en la base de datos
+        const pacienteActualizado = await Paciente.findById(pacientePrueba._id);
+        expect(pacienteActualizado.nombre).toBe('Nuevo nombre');
+        expect(pacienteActualizado.propietario).toBe('Nuevo propietario');
+        // Verifica otros campos actualizados
+
+        // Limpia la base de datos después de la prueba
+        await Paciente.findByIdAndDelete(pacientePrueba._id);
+    });
+
+    it('Debería cambiar el estado y la fecha de salida de un paciente', async () => {
+        // Crear un veterinario de prueba en la base de datos
+        const veterinarioPrueba = new Veterinario({
+            nombre: 'Veterinario de prueba',
+            // Otros campos de veterinario
+        });
+    
+        // Guardar el veterinario en la base de datos
+        await veterinarioPrueba.save();
+    
+        // Crear un paciente de prueba en la base de datos
+        const pacientePrueba = new Paciente({
+            nombre: 'Paciente a cambiar',
+            propietario: 'Propietario de prueba',
+            email: 'correo@example.com',
+            celular: '123456789',
+            convencional: '987654321',
+            ingreso: new Date(),
+            sintomas: 'Síntomas de prueba',
+            veterinario: veterinarioPrueba._id, // Asignar el veterinario de prueba
+        });
+    
+        // Guardar el paciente en la base de datos
+        await pacientePrueba.save();
+    
+        const req = {
+            params: {
+                id: pacientePrueba._id, // ID del paciente creado
+            },
+            body: {
+                salida: new Date(), // Agregar una fecha de salida en el cuerpo de la solicitud
+            },
+        };
+    
         const res = {
             status: jest.fn().mockReturnThis(),
             json: jest.fn(),
         };
     
-        await registrarPaciente(req, res);
+        await eliminarPaciente(req, res);
     
-        expect(res.status).toHaveBeenCalledWith(200);
-        expect(res.json).toHaveBeenCalledWith({ msg: "Registro exitoso del paciente" });
+        // Verifica que el paciente haya sido actualizado con el estado y la fecha de salida correctos
+        const pacienteActualizado = await Paciente.findById(pacientePrueba._id);
+        expect(pacienteActualizado.estado).toBe(false); // Asegura que el estado sea falso
+        expect(pacienteActualizado.salida).toBeDefined(); // Asegura que la fecha de salida esté definida
     
         // Limpia la base de datos después de la prueba
-        await Paciente.findOneAndDelete({ nombre: 'Paciente de prueba' });
-    });
+        await Veterinario.findByIdAndDelete(veterinarioPrueba._id);
+        //await Paciente.findByIdAndDelete(pacientePrueba._id);
 
-    it('Debería actualizar los datos de un paciente', async () => {
-        // Agregar código de prueba aquí
     });
+    
 
-    it('Debería eliminar un paciente', async () => {
-        // Agregar código de prueba aquí
-    });
+
 });
-
-
-
